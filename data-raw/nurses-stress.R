48e90c372717feba479ebc78e264e0c3cb82ea7e
rikkehjortebjerg <rikke.hjortebjerg@outlook.com>
2026-04-22 10:12:27 +0200

    Download nurses stress data and save to data-raw

---
+++ data-raw/nurses-stress.R
@@ -0,0 +1,11 @@
+library(here)
+
+# The dataset can be downloaded manually from
+# <https://drive.proton.me/urls/ZHZ6TCAN2W#e5D5U4IajdYp>.
+# Make sure to save this file to the `data-raw/` folder with the name
+# `nurses-stress.tar`.
+
+untar(
+  here("data-raw/nurses-stress.tar"),
+  exdir = here("data-raw/nurses-stress/")
+)
